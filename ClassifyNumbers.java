0eb9156775cfa42a05fc8028847044af79983889
Dena-t <denaatorabi@gmail.com>
2023-12-03 01:09:48 -0500

    added if nested in Swtich to check the zeros, combined to cases (case 1, -1)

--- ClassifyNumbers.java
+++ ClassifyNumbers.java
@@ -1,6 +1,5 @@
 package Project;
 
-import java.lang.*;
 import java.util.*;
 public class ClassifyNumbers{
 public static void main(String[] args) {
@@ -10,6 +9,7 @@ public static void main(String[] args) {
     int number;
     int counter;
     int remainder;
+    int i;
     Scanner console;
     
 
@@ -20,32 +20,49 @@ public static void main(String[] args) {
     counter =0;
     remainder =0;
     console = new Scanner(System.in);
+ 
+ 
+   System.out.println("Please enter 20 integers , positive,negaruve or zeros"); 
 
-    
-    
-     System.out.println("Please enter a number:");
-     number = console.nextInt();
-     remainder = number % 2 ;
+   for(i=0;i<=19;i++){
+
+/*Uncomment if you want to ask user to enter the count of numbers manually */
+  // System.out.println("How many integers you want to enter?");
+   // counter = console.nextInt();
+    //for (i=1;i<=counter;i++){
+        System.out.println("Enter number "+ i + "`s value");
+        number = console.nextInt();
+        remainder = number % 2 ;
+ 
     switch (remainder){
         case 0:
-        System.out.println("number is even");
+        System.out.println("number you entered is even");
         evens ++;
+           if (number == 0){
+             System.out.println("Number is 0");
+             zeros++;
+               } 
         break;
-        case 1:
-        System.out.println("number is odd");
+        case 1: case -1:
+        System.out.println("number you entered is odd");
         odds ++;
         break;
 
-        case -1:
-        System.out.println("number is odd and negative");
-        odds ++;
-        break;
         }
-         
+    console.close();
+}
+
+        System.out.printf("%s %d %s %d %s %n","there are",evens,"evens which also includes ",zeros,"zeros" );
+         System.out.println();
+         System.out.println("Total number of odds is "+ odds);
+       
+ 
   
+   }
+
 
 }
-}
+//}
 
 
 
