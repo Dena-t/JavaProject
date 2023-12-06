bfe349f89e78e0288e6ca1a7117f84ecc7965a1d
Carlobunn-123 <151385151+Carlobunn-123@users.noreply.github.com>
2023-12-06 15:41:23 -0500

    Add files via upload

---
+++ MovieTicketSalev1.java
@@ -0,0 +1,67 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MovieTicketSalev1 {
+
+    public static void main(String[] args) {
+
+        String movieName;
+        double adultTicketPrice;
+        double childTicketPrice;
+        int noOfAdultTicketSold;
+        int noOfChildTicketSold;
+        double percentDonated;
+        double amountDonated;
+        double grossAmount;
+        double netSaleAmount;
+        Scanner console;
+        int numberOfTicketSold;
+
+        
+        try{
+        console = new Scanner(System.in);
+
+        System.out.println("Please enter the following needed info: ");
+        System.out.print("Movie Name: ");
+        movieName = console.nextLine();
+
+
+        System.out.print("Adult Ticket Price: ");
+        adultTicketPrice = console.nextDouble();
+
+        System.out.print("Child Ticket Price: ");
+        childTicketPrice = console.nextDouble();
+
+        System.out.print("Number of Adult Ticket sold: ");
+        noOfAdultTicketSold = console.nextInt();
+
+        System.out.print("Number of child Ticket sold: ");
+        noOfChildTicketSold = console.nextInt();
+
+        System.out.print("Percentage of the gross amount to be donated to the charity: ");
+        percentDonated = console.nextDouble();
+        console.close();
+
+        grossAmount = adultTicketPrice * noOfAdultTicketSold + childTicketPrice*noOfChildTicketSold;
+        amountDonated = grossAmount * percentDonated / 100;
+        netSaleAmount = grossAmount - amountDonated;
+        numberOfTicketSold = noOfAdultTicketSold + noOfChildTicketSold;
+        System.out.println("========================================");
+        System.out.printf("%s %s %n%s %d %n%s $%.2f %n%s %.2f%% %n%s $%.2f %n%s $%.2f %n" 
+        ,"Movie Name:", movieName, "Number of Ticket Sold:",
+        numberOfTicketSold,"Gross Amount :",grossAmount,
+        "Percentage of the Gross Amount Donated:", percentDonated ,"Amount Donated:",amountDonated
+        , "Net Sale:", netSaleAmount);
+        System.out.println("========================================");
+        }
+        catch(InputMismatchException ex){
+            System.out.println(ex);
+            System.out.println("Please re-input all your information with right form./n");
+        }
+        
+    }
+    }
+
+
+    
